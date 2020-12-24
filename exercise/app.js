8f68d9d5102eebc9c04be4b1896c732df328834a
Rami David <56394661+RDmaz@users.noreply.github.com>
2020-12-24 11:05:48 +0100

    added new post logic

--- exercise/app.js
+++ exercise/app.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+// למה אצל נאז האקספרס מופיע בכחול?
 
 app.get("/api/posts", (req, res) => {
 	res.status(200).send(postsData.get());
@@ -25,10 +26,32 @@ app.get("/api/posts/:post_id", (req, res) => {
 	}
 });
 
+app.get("/api/posts/:post_id", (req, res) => {
+	const postId = req.params.post_id;
+	const foundPost = postData.getIndividualBlog(postId);
+	if (foundPost) {
+		res.status(200).send(foundPost);
+	} else {
+		res.status(404).send("Not Found");
+	}
+});
+
+app.post("/api/posts", (req, res) => {
+	const newPost = {
+		id: `${Date.now()}`,
+		title: req.body.title,
+		content: req.body.content,
+		post_image: req.body["post-image"],
+		added_date: `${Date.now()}`,
+	};
+	postsData.add(newPost);
+	res.status(201).send(newPost);
+});
+
 app.listen(3000, () => console.log("Listening on http://localhost:3000")); //initialize the server
 
-/* with the lines bellow above res.status(200) we've tested putting a new blog to data.json file.
-const test = {
-	testing: "testing"
-}
-postsData.add(test); */
+// with the lines bellow above res.status(200) we've tested putting a new blog to data.json file.
+// const test = {
+// 	testing: "testing"
+// }
+// postsData.add(test)
